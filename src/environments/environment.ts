b764a3baea219862c7026ae8a81b6025de19b420
Shkarupa Daniil <zshkarrr@outlook.com>
2023-06-17 00:09:06 +0300

    Update env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
 	production: true,
-	basePathApi: 'https://wondrlink-back-zshkarrr-gmailcom-wondrlink.vercel.app/api/',
+	basePathApi: 'https://wondrlink-back-zshkarrr-gmailcom-wondrlink.vercel.app/api',
 	authPathApi: 'https://wondrlink-back-zshkarrr-gmailcom-wondrlink.vercel.app/api/auth'
 };
