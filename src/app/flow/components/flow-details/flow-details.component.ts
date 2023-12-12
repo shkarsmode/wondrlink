dedadfb290d676c49b58259d322b1fae50c8edec
Shkarupa Daniil <zshkarrr@outlook.com>
2023-12-12 23:18:07 +0200

    add form flow submit only for modal

--- src/app/flow/components/flow-details/flow-details.component.ts
+++ src/app/flow/components/flow-details/flow-details.component.ts
@@ -1,7 +1,12 @@
-import { ChangeDetectorRef, Component, Input } from '@angular/core';
+import { ChangeDetectorRef, Component, Input, Optional} from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { FlowDataService } from 'src/app/shared/services/flow-data.service';
 import { FlowData } from '../../flow.component';
+import { MatDialog, MatDialogConfig, MatDialogRef } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { FlowDialogComponent } from '../flow-dialog/flow-dialog.component';
+import { CheckEmailComponent } from 'src/app/shared/dialogs/check-email/check-email.component';
+import { AuthService } from 'src/app/shared/services/auth.service';
 
 @Component({
   selector: 'app-flow-details',
@@ -24,16 +29,24 @@ export class FlowDetailsComponent {
   public isCancerTypeSelect: boolean = false;
   public cancerTypes: string[];
 
+  private dialogConfig: MatDialogConfig = new MatDialogConfig()
+
+
   constructor(
     private flowDataService: FlowDataService,
     private fb: FormBuilder,
-    private detRef: ChangeDetectorRef
+    private authService: AuthService,
+    private detRef: ChangeDetectorRef,
+    private dialog: MatDialog,
+    private errorSnackBar: MatSnackBar,
+    @Optional() private dialogRef: MatDialogRef<FlowDialogComponent>
   ){}
 
   ngOnInit(): void {
     this.initDetailsForm();
     this.initCancerTypes();
     this.onDiseaseCategoryChange();
+    this.initDialogConfig();
   }
 
   ngAfterContentChecked(): void {
@@ -73,11 +86,41 @@ export class FlowDetailsComponent {
     this.cancerTypes = this.flowDataService.getCancerTypes();
   }
 
+  private initDialogConfig(): void {
+    this.dialogConfig.width = '630px';
+    this.dialogConfig.height = '500px';
+    this.dialogConfig.disableClose = true;
+  }
+
   public onFormSubmit(): void {
     const cancerTypeValue = this.detailsForm.get('cancerType')?.value;
 
     let body = Object.assign(this.detailsForm.value, ...this.flowData, {cancerType: cancerTypeValue || null });
-    console.log(body);
+
+    this.authService.registration(body)
+    .subscribe({
+        next: res => {
+            console.log(res);
+            this.isSending = false;
+            this.openCheckEmailModalWindow();
+        },
+        error: error => {
+            console.log(error.message);
+            this.errorSnackBar.open(error.message, '', {
+                verticalPosition: "top"
+            })
+            this.isSending = false;
+        }
+    });
   }
+
+  public openCheckEmailModalWindow(): void {
+    this.dialog.open(CheckEmailComponent, this.dialogConfig);
+
+    // Check if FlowDialogComponent is open as a MatDialog before attempting to close it
+    if (this.dialogRef && this.dialogRef.componentInstance) {
+        this.dialogRef.close();
+    }
+}
 
 }
