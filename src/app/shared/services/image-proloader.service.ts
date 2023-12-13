2c5570edf1219e75b3282d3a510ea0b208bfb64c
Daniil Shkarupa <d.shkarupa@boosteroid.com>
2023-12-13 16:42:14 +0200

    Update image preloader service to preload material symbols to show them immediately

--- src/app/shared/services/image-proloader.service.ts
+++ src/app/shared/services/image-proloader.service.ts
@@ -1,10 +1,9 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-    providedIn: 'root'
+    providedIn: 'root',
 })
 export class ImageProloaderService {
-
     private readonly path: string = 'assets/img/';
     private readonly images: string[] = [
         'bg.png',
@@ -22,16 +21,28 @@ export class ImageProloaderService {
         'ecosystem-icon.png',
         'close.svg',
         'arrowback.svg',
-        'dropdown.png'
+        'dropdown.png',
         // 'union.png'
+    ];
+    private readonly materialSymbols: string[] = [
+        'location_city',
+        'sync',
+        'west',
+        'east',
+        'file_copy',
+        'library_add_check',
+        'visibility_off',
+        'visibility',
+        'keyboard_backspace',
+        'close'
     ];
     private loadedLenght: number = 0;
 
-    constructor() { }
+    constructor() {}
 
     public isloadedAll(): boolean {
         this.loadedLenght++;
-        if (this.images.length === this.loadedLenght){
+        if (this.images.length === this.loadedLenght) {
             return true;
         }
 
@@ -39,7 +50,10 @@ export class ImageProloaderService {
     }
 
     public getAllBGImagesPath(): string[] {
-        return this.images.map(image => this.path + image);
+        return this.images.map((image) => this.path + image);
     }
 
+    public getAllMaterialSymbols(): string[] {
+        return this.materialSymbols;
+    }
 }
