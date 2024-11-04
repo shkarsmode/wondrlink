f2ae5cafc5b96ac812f5c26f66cabea098fc930b
Daniil Shkarupa <d.shkarupa@boosteroid.com>
2024-11-04 12:24:01 +0200

    Update

--- src/app/shared/interfaces/IInfo.ts
+++ src/app/shared/interfaces/IInfo.ts
@@ -1,7 +1,8 @@
 export interface IInfo {
     readonly id: string;
-	readonly header: string;
-	readonly intro: string;
-	readonly subHeader: string;
-	readonly content: string;
+    readonly header: string;
+    readonly intro: string;
+    readonly subHeader: string;
+    readonly content: string;
+    readonly buttonName?: string;
 }
--- src/app/shared/services/article-service.service.ts
+++ src/app/shared/services/article-service.service.ts
@@ -1,73 +1,70 @@
-import { FlowComponentConfig, TFormFlow } from 'src/app/shared/interfaces/TFormFlow';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
-import { IInfo } from '../interfaces/IInfo';
 import { MatDialog } from '@angular/material/dialog';
+import { BehaviorSubject } from 'rxjs';
 import { FlowDialogComponent } from 'src/app/flow/components/flow-dialog/flow-dialog.component';
+import { FlowComponentConfig, TFormFlow } from 'src/app/shared/interfaces/TFormFlow';
+import { IInfo } from '../interfaces/IInfo';
 
 export type TArticleFormState = 'active' | 'hidden';
 @Injectable({
-    providedIn: 'root'
+    providedIn: 'root',
 })
 export class ArticleService {
-    private _ArticleFormSubject = new BehaviorSubject<TArticleFormState>('active');
+    private _ArticleFormSubject = new BehaviorSubject<TArticleFormState>(
+        'active'
+    );
     public articleForm$ = this._ArticleFormSubject.asObservable();
 
-    private formTitle: {[key in TFormFlow]: string} = {
-        'patients': "We are fighting together, please sign up here!",
-        'drug-developers': "We are fighting together, join us!",
-        'physicians': "We are fighting together, please sign up here!",
-        'ecosystem': "We are fighting together, join us!",
-    }
+    private formTitle: { [key in TFormFlow]: string } = {
+        patients: 'We are fighting together, please sign up here!',
+        'drug-developers': 'Help Patients Access Life-Saving Therapies',
+        physicians: 'Help Patients Access Life-Saving Treatments',
+        ecosystem: 'We are fighting together, join us!',
+    };
 
     public info: IInfo[];
 
-
-    constructor(
-        private http: HttpClient,
-        private dialog: MatDialog,
-    ) { }
+    constructor(private http: HttpClient, private dialog: MatDialog) {}
 
     public updateArticleFormState(state: TArticleFormState): void {
         this._ArticleFormSubject.next(state);
     }
 
     public reinitArticleForm(): void {
-        this.updateArticleFormState('hidden')
+        this.updateArticleFormState('hidden');
         setTimeout(() => {
             this.updateArticleFormState('active');
         }, 1000);
     }
 
     public setAllArticles(): void {
-        this.http.get('assets/data/info.json')
-            .subscribe((info: any) => this.info = info.data
-        );
+        this.http
+            .get('assets/data/info.json')
+            .subscribe((info: any) => (this.info = info.data));
     }
 
     public getCurrentArticle(id: string): IInfo {
-        return this.info?.filter(info =>
-            info.id === id
-        )[0];
+        return this.info?.filter((info) => info.id === id)[0];
     }
 
-    public getFormTitleById(id: TFormFlow): string   {
+    public getFormTitleById(id: TFormFlow): string {
         return this.formTitle[id];
     }
 
-    public openFlowDialog(flowConfig: FlowComponentConfig = {
+    public openFlowDialog(
+        flowConfig: FlowComponentConfig = {
             flow: 'patients',
             step: 1,
-            isSkipFirstStep: false
-        }): void {
-        
+            isSkipFirstStep: false,
+        }
+    ): void {
         this.updateArticleFormState('hidden');
 
         let dialogRef = this.dialog.open(FlowDialogComponent, {
             width: '850px',
             maxWidth: '100dvw',
-            data: {...flowConfig},
+            data: { ...flowConfig },
         });
 
         dialogRef.afterClosed().subscribe(() => {
