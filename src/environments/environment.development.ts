234ddb85273f420767c10a487ef841220ee5455d
Daniil Shkarupa <d.shkarupa@boosteroid.com>
2024-11-11 13:48:46 +0200

    Update env

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,10 @@
 export const environment = {
     production: false,
-    basePathApi: 'http://localhost:3000/api',
-    authPathApi: 'http://localhost:3000/api/auth',
+    basePathApi: 'https://wondrlink-back.vercel.app/api',
+    authPathApi: 'https://wondrlink-back.vercel.app/api/auth',
     geoPathAPI: 'https://api.geoapify.com/v1/geocode/autocomplete',
     geoApiKey: '3b23dc57405b446fbbf29846a2d2b6dd',
 };
+
+//basePathApi: 'http://localhost:3000/api',
+    // authPathApi: 'http://localhost:3000/api/auth',
