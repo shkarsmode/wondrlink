6f7990579c9f2b5cf7d51931481d56b65c95dbee
shkarsmode <zshkarrr@gmail.com>
2025-05-22 12:14:08 +0300

    Update

--- api/index.js
+++ api/index.js
@@ -1,10 +1,10 @@
-import("../dist/wondrlink/server/server.mjs")
+import("../dist/wondrlink/server/main.mjs")
     .then((module) => module.app)
     .catch((error) => {
         console.error("Failed to load server module:", error);
         throw error;
     });
 export default async (req, res) => {
-    const { app } = await import("../dist/wondrlink/server/server.mjs");
+    const { app } = await import("../dist/wondrlink/server/main.mjs");
     return app(req, res);
 };
