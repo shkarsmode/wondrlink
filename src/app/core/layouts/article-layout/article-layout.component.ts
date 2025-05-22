266acecf72b11785d2cec6bb952375a737ad7291
shkarsmode <zshkarrr@gmail.com>
2025-05-22 13:23:19 +0300

    Fix tabs

--- src/app/core/layouts/article-layout/article-layout.component.ts
+++ src/app/core/layouts/article-layout/article-layout.component.ts
@@ -30,10 +30,13 @@ export class ArticleLayoutComponent implements OnInit {
 
     public ngOnInit(): void {
         this.getArticleByIdFromRouteParams();
+        this.route.paramMap.subscribe((params) => {
+            this.getArticleByIdFromRouteParams();
+        });
     }
 
     private getArticleByIdFromRouteParams(): void {
-        this.articleId = this.route.snapshot.paramMap.get('articleId') || 'patients'
+        this.articleId = this.route.snapshot.paramMap.get('articleId') || 'patients';
         this.getCurrentArticle();
         this.setupFlowConfig()
     }
